2d6812670ae39f42d6e7ffb4b671e59d1db986ee
Borna Butkovic <borna@favicode.net>
2021-05-21 13:00:29 +0200

    make even api

--- api/index.go
+++ api/index.go
@@ -3,9 +3,23 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	number := r.URL.Query().Get("number")
-	fmt.Fprintf(w, "%s", number)
+	number, err := strconv.Atoi(r.URL.Query().Get("number"))
+
+	if err != nil {
+		fmt.Fprintf(w, "number not integer")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if number%2 == 0 {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "true")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "false")
+	}
 }
